7075c6c66fe2adf5e4c9e8653f39a4cf2f90abbb
takurx <taku3wzr@gmail.com>
2017-03-03 17:35:13 +0900

    first commit
    first commit

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ClickCountUp
+{
+    public partial class Form1 : Form
+    {
+        int Count;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Count = 0;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Count = Count + 1;
+            string StringCount = Count.ToString();
+            label1.Text = StringCount;
+        }
+    }
+}
